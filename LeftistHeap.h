19b016b458a26a5856b2c4138fd9c90d200fc5be
hhchaudh <haaris.h.chaudhry@gmail.com>
2017-03-16 22:19:55 -0500

    Added comments to the Leftist Heap files

--- LeftistHeap.h
+++ LeftistHeap.h
@@ -1,3 +1,10 @@
+/**
+*	@file : LeftistHeap.h
+*	@author :  Haaris Chaudhry
+*	@date : Mar 16, 2017
+*	Purpose: This class will perform the functions of a leftist heap
+*/
+
 #ifndef LEFTIST_HEAP_H
 #define LEFTIST_HEAP_H
 
@@ -7,21 +14,82 @@
 class LeftistHeap
 {
 public:
+    /**
+    * Constructor for the Leftist Heap
+    * @return A Leftist Heap with a null root
+    */
     LeftistHeap();
+
+    /**
+    * Destructor for the Leftist Heap
+    */
     ~LeftistHeap();
+
+    /**
+    * The insertion function will take a value and insert it into the heap using the merge()
+    * @param aKey The value to be inserted
+    */
     void insert( long aKey );
+
+    /**
+    * The deleteMin function will delete the minimum value and will then use merge() to merge the two remaining subtrees
+    * @param aKey The value to be inserted
+    */
     void deleteMin();
+
+    /**
+    * This function prints the values in the heap using a preorder traversal
+    */
     void preorderPrint();
+
+    /**
+    * This function prints the values in the heap using an inorder traversal
+    */
     void inorderPrint();
+
+    /**
+    * This function prints the values in the heap using a levelorder traversal
+    */
     void levelorderPrint();
 private:
+    /**
+    * This function prints the values in the heap using a preorder traversal
+    * @param aNodePtr The root of the current subtree
+    */
     void preorderPrint( LeftistNode* aNodePtr );
+
+    /**
+    * This function prints the values in the heap using a inorder traversal
+    * @param aNodePtr The root of the current subtree
+    */
     void inorderPrint(LeftistNode* aNodePtr);
+
+    /**
+    * This function will merge two tree together
+    * @param H1 The root of the first subtree
+    * @param H2 The root of the second subtree
+    */
     LeftistNode* merge( LeftistNode* H1, LeftistNode* H2 );
+
+    /**
+    * This function is used by the destructor to delete all nodes in the tree
+    * @param aNodePtr The root of the current subtree
+    */
     void postOrderDelete(LeftistNode* aNodePtr);
+
+    /**
+    * This function will swap two trees
+    * @param H1 The root of the first subtree
+    * @param H2 The root of the second subtree
+    */
     void swap( LeftistNode*& H1, LeftistNode*& H2 );
+
+    /**
+    * This function will swap the children of a node
+    * @param H1 The node with children that need to be swapped
+    */
     void swapChildren( LeftistNode*& H1 );
 
-    LeftistNode* mRoot;
+    LeftistNode* mRoot;         //!< The root of the heap
 };
 #endif // !LEFTIST_HEAP_H
--- LeftistNode.h
+++ LeftistNode.h
@@ -1,22 +1,74 @@
+/**
+*	@file : LeftistNode.h
+*	@author :  Haaris Chaudhry
+*	@date : Mar 16, 2017
+*	Purpose: This class will perform the functions of a leftist node
+*/
 #ifndef LEFTIST_NODE_H
 #define LEFTIST_NODE_H
 
 class LeftistNode
 {
 public:
+    /**
+    * The leftist node constructor
+    * @param aKey The value that the node will store in mKey
+    * @return A node with aKey as its internal mKey
+    */
     LeftistNode(long aKey);
+
+    /**
+    * This function retrieves the rank of the node
+    * @return The rank of the node
+    */
     long getRank();
+
+    /**
+    * This function return the value stored in mKey
+    * @return The value stored in mKey
+    */
     long getKey();
+
+    /**
+    * Sets the left child of the node
+    * @param aLeftChild A pointer pointing to a node that will be assigned to mLeftChild
+    */
     void setLeftChild( LeftistNode* aLeftChild );
+
+    /**
+    * Sets the right child of the node
+    * @param aRightChild A pointer pointing to a node that will be assigned to mRightChild
+    */
     void setRightChild( LeftistNode* aRightChild );
+
+    /**
+    * Retrieves a copy of mLeftChild
+    * @return A copy of mLeftChild
+    */
     LeftistNode* getLeftChild();
+
+    /**
+    * Retrieves a copy of mRightChild
+    * @return A copy of mRightChild
+    */
     LeftistNode* getRightChild();
+
+    /**
+    * Adjusts the rank of the current node
+    */
     void adjustRank();
 private:
+    /**
+    * Calculates the minimum of two values
+    * @param aVal1 The first value to be compared
+    * @param aVal2 The second value to be compared
+    * @return The value that is the smallest
+    */
     long minVal( long aVal1, long aVal2 );
-    long mRank;
-    long mKey;
-    LeftistNode* mLeftChild;
-    LeftistNode* mRightChild;
+
+    long mRank;                 //!< The rank of the node
+    long mKey;                  //!< The value contained within the node
+    LeftistNode* mLeftChild;    //!< A pointer to the left child of the node
+    LeftistNode* mRightChild;   //!< A pointer to the right child of the node
 };
 #endif
